cc9c365cf2437e1388a086154c28bfdd3b7f7ab4
Akiah Tullis <42794497+Ill-Satisfaction@users.noreply.github.com>
2025-06-18 14:00:03 -0700

    refactor: centralize styles

--- app/grocery-plan.tsx
+++ app/grocery-plan.tsx
@@ -2,12 +2,12 @@ import { Text, View } from "react-native";
 import NavBar from "../components/NavBar";
 import { styles } from "../styles";
 
-export default function Index() {
+export default function GroceryPlan() {
   return (
     <View style={styles.page}>
       <NavBar />
       <View style={styles.contentCenter}>
-        <Text>Welcome!</Text>
+        <Text>Grocery Plan Page</Text>
       </View>
     </View>
   );
